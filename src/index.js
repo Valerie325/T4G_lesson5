e39e0b06624bb1090dff139632e3eea26ee9ea18
Valerie325 <morevimgh14@gmail.com>
2024-12-09 12:47:12 +0000

    js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// let head = document.getElementById("heading")
+// head.innerHTML ="I am going to miss you all ✌️"
+// let img =document.getElementById("img")
+// img.innerHTML ="<img src'../images/blueberry.jfif"   
+
+function changeButtonStyle() {
+
+    button.style.backgroundColor = "pink";
+    button.style.backgroundcolor = "green";
+}
+
+let button = document.querySelector("button")
+button.addEventListener("click", changeButtonStyle);
+
+
+function changepicStyle() {
+    
+    pic.style.querySelector 
+}
+
+let pic = document.querySelector("pic");
+pic.addEventListener("click",)
