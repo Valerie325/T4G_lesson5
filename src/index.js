62079e2505ce76222528e03baef59ab9ae96c1c6
Valerie325 <morevimgh14@gmail.com>
2024-12-10 13:53:32 -1200

    made changes

--- src/index.js
+++ src/index.js
@@ -3,20 +3,36 @@
 // let img =document.getElementById("img")
 // img.innerHTML ="<img src'../images/blueberry.jfif"   
 
-function changeButtonStyle() {
+// function changeButtonStyle() {
 
-    button.style.backgroundColor = "pink";
-    button.style.backgroundcolor = "green";
+//     button.style.backgroundColor = "pink";
+//     button.style.backgroundcolor = "green";
+// }
+
+// let button = document.querySelector("button")
+// button.addEventListener("click", changeButtonStyle);
+
+
+let button =document.querySelector("button");
+function openImage() {
+    let image = document.querySelector(".img")
+    image.style.display = "block"
+    let close = document.querySelector(".close")
+    close.style.display = "block"
+    let button = document.querySelector("button")
+    button.style.display = "none"
+
+}
+button.addEventListener("click", openImage,);
+
+let close = document.querySelector(".close");
+function closeImage(){
+    let image = document.querySelector(".img")
+    image.style.display = "none"
+    let close = document.querySelector(".close")
+    close.style.display = "none"
+    let button = document.querySelector("button")
+    button.style.display = "block"
 }
 
-let button = document.querySelector("button")
-button.addEventListener("click", changeButtonStyle);
-
-
-function changepicStyle() {
-    
-    pic.style.querySelector 
-}
-
-let pic = document.querySelector("pic");
-pic.addEventListener("click",)
+close.addEventListener("click", closeImage);
